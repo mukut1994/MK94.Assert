6de5488f3c4a87edf9d6e641783989ad443f362f
Mithilesh Zavar <mithileshz@hotmail.co.uk>
2022-01-28 17:57:15 +0000

    Add Newtonsoft Serializer Unit Test

--- MK94.Assert.NUnit.Test/SerializerTest.cs
+++ MK94.Assert.NUnit.Test/SerializerTest.cs
@@ -6,7 +6,7 @@ namespace MK94.Assert.NUnit.Test
     public class SerializerTest
     {
         [Test]
-        public void CustomSerializer()
+        public void CustomSerializerOptions()
         {
             var serializerOptions = new JsonSerializerOptions { WriteIndented = false };
             
@@ -22,6 +22,23 @@ namespace MK94.Assert.NUnit.Test
             };
 
             asserter.Matches("Step 1", fileContent);
-        }        
+        }
+        
+        [Test]
+        public void CustomSerializer_Newtonsoft()
+        {
+            var asserter = SetupDiskAssert.InstanceWithRecommendedSettings("MK94.Assert", "CustomSerializer")
+                .WithDevModeOnEnvironmentVariable("NONE", "NONE")
+                .WithSerializer(Newtonsoft.Json.JsonConvert.SerializeObject);
+
+            var fileContent = new
+            {
+                Name = "Name",
+                Text = "Text",
+                Number = 1
+            };
+
+            asserter.Matches("Step 1", fileContent);
+        }
     }
 }
