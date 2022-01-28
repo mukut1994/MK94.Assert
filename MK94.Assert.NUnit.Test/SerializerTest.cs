0c3950b61ce8ea19a56b5c87b74d1ae05c0a7366
Mithilesh Zavar <mithileshz@users.noreply.github.com>
2022-01-28 18:00:47 +0000

    Add Newtonsoft Serializer Unit Test (#21)
    * Add Newtonsoft Serializer Unit Test
    
    * Change Tests

--- MK94.Assert.NUnit.Test/SerializerTest.cs
+++ MK94.Assert.NUnit.Test/SerializerTest.cs
@@ -6,13 +6,14 @@ namespace MK94.Assert.NUnit.Test
     public class SerializerTest
     {
         [Test]
-        public void CustomSerializer()
+        public void CustomSerializerOptions()
         {
             var serializerOptions = new JsonSerializerOptions { WriteIndented = false };
             
             var asserter = SetupDiskAssert.InstanceWithRecommendedSettings("MK94.Assert", "CustomSerializer")
                 .WithDevModeOnEnvironmentVariable("NONE", "NONE")
-                .WithSerializer(x => JsonSerializer.Serialize(x, serializerOptions));
+                .WithSerializer(x => JsonSerializer.Serialize(x, serializerOptions))
+                .EnableWriteMode();
 
             var fileContent = new
             {
@@ -22,6 +23,23 @@ namespace MK94.Assert.NUnit.Test
             };
 
             asserter.Matches("Step 1", fileContent);
-        }        
+        }
+        
+        [Test]
+        public void CustomSerializer_Newtonsoft()
+        {
+            var asserter = SetupDiskAssert.InstanceWithRecommendedSettings("MK94.Assert", "CustomSerializer")
+                .WithDevModeOnEnvironmentVariable("NONE", "NONE")
+                .WithSerializer(Newtonsoft.Json.JsonConvert.SerializeObject);
+
+            var fileContent = new
+            {
+                Name = "Name",
+                Text = "Text",
+                Number = 1
+            };
+
+            asserter.Matches("Step 1", fileContent);
+        }
     }
 }
